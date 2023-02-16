573b4a722a8226b1a5f0de98ca50a0bb9a8b51a8
RolanAbbasguliev <abbarolan@gmail.com>
2023-02-16 16:09:05 +0300

    init project

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,10 @@
+import Header from '@/Components/App/Header/Header'
+const App = () => {
+  return (
+    <div className='w-screen h-screen bg-[#210E09]'>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/App/Header/Header.tsx
@@ -0,0 +1,42 @@
+import Logo from '../../../assets/Img/Logo.png'
+
+const Header = () => {
+  return (
+    <header className='pt-[30px]'>
+      <div
+        className='
+          wrapper w-[1300px] h-[51px]
+          flex justify-between items-center
+          text-white
+          mx-auto
+        '
+      >
+        <img src='{Logo}' alt='Logo' className='logo' />
+        <nav className='menu'>
+          <ul
+            className='
+            menu_bar flex
+            gap-[30px]
+          '
+          >
+            <li className='menu_item'>Главная</li>
+            <li className='menu_item'>Меню</li>
+            <li className='menu_item'>Афиша</li>
+            <li className='menu_item'>Сотрудничевство</li>
+            <li className='menu_item'>Галерея</li>
+            <li className='menu_item'>Новости</li>
+          </ul>
+        </nav>
+        <button
+          className='
+          booking_btn flex justify-end
+          '
+        >
+          Бронирование
+        </button>
+      </div>
+    </header>
+  )
+}
+
+export default Header
