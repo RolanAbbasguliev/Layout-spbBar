c4cf9c9c58fe837b65f7e47fe876cbfbe4e9b477
RolanAbbasguliev <rolangames@gmail.com>
2023-02-16 16:19:05 +0300

    header done

--- src/Components/App/Header/Header.tsx
+++ src/Components/App/Header/Header.tsx
@@ -1,42 +1,42 @@
-import Logo from '../../../assets/Img/Logo.png'
+import Logo from "../../../assets/Img/Logo.png";
 
 const Header = () => {
   return (
-    <header className='pt-[30px]'>
+    <header className="pt-[30px]">
       <div
-        className='
+        className="
           wrapper w-[1300px] h-[51px]
           flex justify-between items-center
           text-white
-          mx-auto
-        '
+          mx-auto my-0`
+        "
       >
-        <img src='{Logo}' alt='Logo' className='logo' />
-        <nav className='menu'>
+        <img src={Logo} width="157" height="40" alt="Logo" className="logo" />
+        <nav className="menu">
           <ul
-            className='
+            className="
             menu_bar flex
             gap-[30px]
-          '
+          "
           >
-            <li className='menu_item'>Главная</li>
-            <li className='menu_item'>Меню</li>
-            <li className='menu_item'>Афиша</li>
-            <li className='menu_item'>Сотрудничевство</li>
-            <li className='menu_item'>Галерея</li>
-            <li className='menu_item'>Новости</li>
+            <li className="menu_item">Главная</li>
+            <li className="menu_item">Меню</li>
+            <li className="menu_item">Афиша</li>
+            <li className="menu_item">Сотрудничевство</li>
+            <li className="menu_item">Галерея</li>
+            <li className="menu_item">Новости</li>
           </ul>
         </nav>
         <button
-          className='
+          className="
           booking_btn flex justify-end
-          '
+          "
         >
           Бронирование
         </button>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
